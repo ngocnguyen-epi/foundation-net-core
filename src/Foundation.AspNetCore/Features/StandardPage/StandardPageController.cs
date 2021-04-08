707ea452a891d95e58a0d84da81b64a09ae1010d
Trung Do <trung.do@episerver.com>
2021-04-08 12:07:14 +0700

    add standard page

---
+++ src/Foundation.AspNetCore/Features/StandardPage/StandardPageController.cs
@@ -0,0 +1,23 @@
+﻿using EPiServer.DataAbstraction;
+using EPiServer.Web.Mvc;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Foundation.AspNetCore.Features.StandardPage
+{
+    public class StandardPageController : PageController<StandardPage>
+    {
+        private readonly CategoryRepository _categoryRepository;
+
+        public StandardPageController(CategoryRepository categoryRepository)
+        {
+            _categoryRepository = categoryRepository;
+        }
+
+        //[PageViewTracking]
+        public IActionResult Index(StandardPage currentPage)
+        {
+            var model = StandardPageViewModel.Create(currentPage, _categoryRepository);
+            return View(model);
+        }
+    }
+}

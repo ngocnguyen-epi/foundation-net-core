a94d2dde83db9285afcacdb8f94173fb6aeac2d3
Duc Quan Tran <DucQuan.Tran@episerver.com>
2021-04-08 14:08:23 +0700

    add CallToAction block

---
+++ src/Foundation.AspNetCore/Features/Blocks/CallToActionBlock/CallToActionBlockSelectionFactory.cs
@@ -0,0 +1,23 @@
+﻿using EPiServer.Shell.ObjectEditing;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Foundation.AspNetCore.Features.Blocks.CallToActionBlock
+{
+    class BackgroundImageSelectionFactory : ISelectionFactory
+    {
+        public IEnumerable<ISelectItem> GetSelections(ExtendedMetadata metadata)
+        {
+            return new List<SelectItem>
+            {
+                new SelectItem { Text = "Fit width", Value = "image-fit-width" },
+                new SelectItem { Text = "Fit height", Value = "image-fit-height" },
+                new SelectItem { Text = "Stretch", Value = "image-stretch" },
+                new SelectItem { Text = "Tile", Value = "image-tile" },
+                new SelectItem { Text = "Default", Value = "image-default" }
+            };
+        }
+    }
+}

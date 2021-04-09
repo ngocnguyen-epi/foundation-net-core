6dacd96d2af9e60e5f8230e73750aa288c04fd7a
Duc Quan Tran <DucQuan.Tran@episerver.com>
2021-04-09 10:37:44 +0700

    add RssReader block

---
+++ src/Foundation.AspNetCore/Features/Blocks/RssReaderBlock/RssReaderBlockComponent.cs
@@ -0,0 +1,53 @@
+﻿using EPiServer.Web.Mvc;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Foundation.AspNetCore.Features.Blocks.RssReaderBlock
+{
+    public class RssReaderBlockComponent : BlockComponent<RssReaderBlock>
+    {
+        public override IViewComponentResult Invoke(RssReaderBlock currentBlock)
+        {
+            var viewModel = new RssReaderBlockViewModel
+            {
+                RssList = new List<RssReaderBlockViewModel.RssItem>(),
+                CurrentBlock = currentBlock
+            };
+
+            try
+            {
+                if ((currentBlock.RssUrl != null) && (!currentBlock.RssUrl.IsEmpty()))
+                {
+                    var rssDocument = XDocument.Load(Convert.ToString(currentBlock.RssUrl));
+
+                    var posts = from item in rssDocument.Descendants("item").Take(currentBlock.MaxCount)
+                                select new RssReaderBlockViewModel.RssItem
+                                {
+                                    Title = item.Element("title").Value,
+                                    Url = item.Element("link").Value,
+                                    PublishDate = item.Element("pubDate").Value,
+                                };
+
+                    viewModel.RssList = posts.ToList();
+                    viewModel.HasHeadingText = HasHeadingText(currentBlock);
+                    viewModel.Heading = currentBlock.Heading;
+                    viewModel.DescriptiveText = currentBlock.MainBody;
+                }
+            }
+            catch (Exception)
+            {
+                viewModel.HasHeadingText = true;
+                viewModel.Heading = "Invalid RSS Feed URL.";
+            }
+            var view = View(viewModel);
+            view.ViewName = "~/Features/Blocks/RssReaderBlock/RssReaderBlock.cshtml";
+            return view;
+        }
+
+        private bool HasHeadingText(RssReaderBlock currentBlock) => ((!string.IsNullOrEmpty(currentBlock.Heading)) || ((currentBlock.MainBody != null) && (!currentBlock.MainBody.IsEmpty)));
+    }
+}
---
+++ src/Foundation.AspNetCore/Features/Blocks/RssReaderBlock/RssReaderBlockViewModel.cs
@@ -0,0 +1,25 @@
+﻿using EPiServer.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Foundation.AspNetCore.Features.Blocks.RssReaderBlock
+{
+    public class RssReaderBlockViewModel
+    {
+        public RssReaderBlock CurrentBlock { get; set; }
+
+        public XhtmlString DescriptiveText { get; set; }
+        public bool HasHeadingText { get; set; }
+        public string Heading { get; set; }
+        public List<RssItem> RssList { get; set; }
+
+        public class RssItem
+        {
+            public string Title { get; set; }
+            public string Url { get; set; }
+            public string PublishDate { get; set; }
+        }
+    }
+}
